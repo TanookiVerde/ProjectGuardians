e35eb24a8e911782646b609bccbca59a6e7da7f1
vitormilioni <latios1000@gmail.com>
2016-07-28 18:03:35 -0300

    prototipo2

---
+++ Guardians/Assets/Scripts/BombBehaviour.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BombBehaviour : MonoBehaviour {
+
+	//acessa componentes
+
+
+	void Start()
+	{
+		
+	}
+
+	void Update () 
+	{
+		if (transform.position.x < -9) 
+		{
+			Destroy (gameObject);
+		}
+		transform.Translate (new Vector3 (-3f, 0f, 0f) * Time.deltaTime);
+	}
+
+	void OnTriggerEnter2D (Collider2D c)
+	{
+		if (c.gameObject.tag == "pickup") 
+		{
+			Destroy (c.gameObject);
+			Destroy (gameObject);
+		} 
+		else if (c.gameObject.tag == gameObject.tag)
+		{
+			Destroy (gameObject);
+		}
+	} 
+}
---
+++ Guardians/Assets/Scripts/GameManager.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+	//Controle do spawn de projeteis
+	private float spawnTimer;
+
+	//Lista da posicao dos spawns
+	public Transform[] spawn;
+
+	//Lista de Bombas
+	public GameObject[] bombPrefab;
+
+	private int selectedSpawn;
+	private int selectedBomb;
+
+	void Start () 
+	{
+		spawnTimer = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		spawnTimer += Time.deltaTime;
+		if (spawnTimer > 2) 
+		{
+			selectedBomb = Random.Range (0, 3);
+			selectedSpawn = Random.Range (0, 5);
+			Instantiate (bombPrefab[selectedBomb], spawn [selectedSpawn].position, spawn [selectedSpawn].rotation);
+			spawnTimer = 0;
+		} 
+	}
+}
